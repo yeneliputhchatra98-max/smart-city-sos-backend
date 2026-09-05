93021206596fff03dab3858aefc07c48a74846d8
YEN ELIPUTHCHATRA <yeneliputhchatra98@gmail.com>
2026-09-06 01:00:35 +0700

    tunh jmr hz ha

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,16 +1,21 @@
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: 465,
+    port: Number(process.env.SMTP_PORT),
     secure: true,
+
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 20000,
+
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
 });
 const sendPasswordResetEmail = async (email, resetUrl) => {
     await transporter.sendMail({
