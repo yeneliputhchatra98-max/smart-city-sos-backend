ecaffd6f5e26c53cde7a6947d8148c6be28258c4
YEN ELIPUTHCHATRA <yeneliputhchatra98@gmail.com>
2026-09-06 00:50:05 +0700

    fixed error me hot mes

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
+    port: 465,
     secure: true,
     auth: {
         user: process.env.SMTP_USER,
