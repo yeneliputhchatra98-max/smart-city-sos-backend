74fc3af123aaebb3fdbbc7bcd857c4c75af10bfd
YEN ELIPUTHCHATRA <yeneliputhchatra98@gmail.com>
2026-09-06 16:06:48 +0700

    fix gmail smtp

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,14 +1,17 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
+    host: "smtp.gmail.com",
+    port: 587,
     secure: false,
-
+    requireTLS: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
 });
 
 const sendPasswordResetEmail = async (email, resetUrl) => {
